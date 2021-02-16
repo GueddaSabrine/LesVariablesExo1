1c578b36940affadc084686b6521bfdb0640772b
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-16 09:04:46 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LesVariablesExo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+            int b = 5;
+            Console.WriteLine(b);
+            int somme = a + b;
+            Console.WriteLine("le résultat est " + somme);
+        }
+    }
+}
