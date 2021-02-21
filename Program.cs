5f777395328f20d4c1b77e579d2adc91316455c0
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-21 12:12:45 +0100

    commit to Vexo1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ namespace LesVariablesExo1
 {
     class Program
     {
+
+        /*Créer deux variables et leur attribuer comme valeurs deux nombres entiers au choix.
+        Afficher les valeurs de ces nombres dans la console dans deux phrases distinctes.
+        Effectuer la somme des nombres puis afficher le résultat dans la console, dans une phrase.*/
+
         static void Main(string[] args)
         {
             int a = 10;
